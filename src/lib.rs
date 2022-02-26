aff7769d3bf867c0fa8377b4d1d587d08c7fcc6b
Galen Guyer <galen@galenguyer.com>
2022-02-25 23:53:21 -0500

    Message derives Clone

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Message {
     pub author: String,
     pub timestamp: u64,
